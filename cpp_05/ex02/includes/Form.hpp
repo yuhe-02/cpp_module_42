93a0047ec46af0d290d92c8dc30e6ec3d9c9dd1b
yuhe-02 <domineter4649@gmail.com>
2025-02-22 08:01:47 +0000

    solve

---
+++ cpp_05/ex02/includes/Form.hpp
@@ -0,0 +1,39 @@
+#ifndef __FORM_HPP__
+#define __FORM_HPP__
+
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+class Bureaucrat;
+
+class Form {
+	private :
+		const std::string name_;
+		bool  isAssigned_;
+		const int signGrade_;
+		const int execGrade_;
+	public :
+		Form();
+		Form(const Form &copy);
+		Form(std::string name, int signGrade, int execGrade);
+		Form(int signGrade, int execGrade);
+		Form(std::string name);
+		Form &operator=(const Form & copy);
+		~Form();
+		class GradeTooHighException: std::exception {
+			public :
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException: std::exception {
+			public :
+				virtual const char *what() const throw();
+		};
+		void beSigned(Bureaucrat &bureaucrat);
+		int getSignGrade( void ) const;
+		int getExecGrade( void ) const;
+		bool getIsAssigned( void ) const;
+		const std::string getName( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Form* form);
+#endif
