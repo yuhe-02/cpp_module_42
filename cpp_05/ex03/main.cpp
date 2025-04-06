e2102903a278a67c37b0df7f04e027df2ed263b2
Yuhei Yamasaki <yyamasak@c6r2s9.42tokyo.jp>
2025-04-06 15:09:31 +0900

    finish cpp05 03

--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -3,6 +3,7 @@
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "AForm.hpp"
+#include "Intern.hpp"
 
 int main(void)
 {
@@ -30,11 +31,17 @@ int main(void)
 	// 	}
 	// }
 	{
-		AForm *a = new ShrubberyCreationForm("home");
-		Bureaucrat *b = new Bureaucrat(1);
-		b->signForm(*a);
+		Intern intern;
 		try {
-			a->execute(*b);
+			AForm *a = intern.makeForm("robotomy request", "Bender");
+			AForm *b = intern.makeForm("shrubbery creation", "home");
+			AForm *c = intern.makeForm("presidential pardon", "Bender");
+			AForm *f = intern.makeForm("nothing", "Bender");
+			Bureaucrat *d = new Bureaucrat(1);
+			d->signForm(*a);
+			d->signForm(*b);
+			d->signForm(*c);
+			std::cout << f << std::endl;
 		} catch (AForm::Exception &e) {
 			std::cout << "failed to exec : " << e.what() << std::endl;
 		}
